9c756862fd7d7426f69137a9b692cff21f914466
Nikhila Balmoori <nbalmoori@gmail.com>
2021-12-17 13:23:11 -0700

    Add dinosaur facts

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+
+var dinosaur = {
+  title: "Facts about Dinosaurs!",
+  era: "Mesozoic",
+  numberOfSpecies: 700,
+  largestSizeInFeet: 150,
+  extinct: true,
+  laysEggs: true,
+  periods: ["triassic", "jurassic", "creaceous"],
+  descendants: ["lizards", "turtles", "snakes"],
+};
