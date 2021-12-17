224678e64a83ed0f0c86b7a835715a59abc01750
Nikhila Balmoori <nbalmoori@gmail.com>
2021-12-17 13:37:42 -0700

    Add developer information git status cd

---
+++ models/modelB.js
@@ -0,0 +1,13 @@
+
+
+
+var developer = {
+  department: "Technology",
+  jobTitle: "Front-end Developer",
+  yearsOfExperienceRequired: 0,
+  salary: 70000,
+  coverLetterRequired: true,
+  degreeRequired: false,
+  responsibilities: ["Application development and support", "Testing and implementation of new systems", "Work with internal customers"],
+  benefits: ["Health insurance", "Glexible workplace options", "10 paid holidays"],
+};
