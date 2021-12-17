2347528e6d183684cffae5ce65c647419a81563a
Nikhila Balmoori <nbalmoori@gmail.com>
2021-12-17 13:08:33 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,11 @@
+var movie = "Forrest Gump"
+var isEntertaining = true
+movie = "10 Things I Hate About You"
+
+var num4 = 4
+var num6 = 6
+var product = num4 * num6
+
+var favoriteFoods = ["wings", "pomegranates", "soup dumplings"]
+favoriteFoods.shift()
+console.log(movie.length);
